62348a7bdfa4c959868e1c04c94abe7eb6141ce1
Bogdan Ungureanu <49443238+daemoohn@users.noreply.github.com>
2020-07-06 01:23:47 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,4 +15,46 @@ if [[ $bintray_response != *"Version '$version' was not found"* ]]; then
   exit 1
 fi
 
-## tbd
+## configureVorbis.sh
+cd vorbis
+if [ ! -f configure ]; then
+  # generate configuration script
+  # disable running configure automatically
+  sed -i.bak 's/$srcdir\/configure/#/' autogen.sh
+  ./autogen.sh
+  
+  # disable oggpack_writealign test
+  sed -i.bak 's/$ac_cv_func_oggpack_writealign/yes/' configure
+fi
+cd ..
+
+## compileVorbisJs.sh
+dir=`pwd`
+
+# set up the build directory
+mkdir -p build
+cd build
+
+mkdir -p js
+cd js
+
+mkdir -p root
+mkdir -p libvorbis
+cd libvorbis
+  
+# finally, run configuration script
+emconfigure ../../../vorbis/configure \
+    --disable-oggtest \
+    --prefix="$dir/build/js/root" \
+    --disable-shared \
+|| exit 1
+
+# compile libvorbis
+emmake make -j4 || exit 1
+emmake make install || exit 1
+
+cd $dir
+
+## upload to bintray
+zip -r libvorbis-ogv.js.zip $dir/build/js/root 
+curl -T libvorbis-ogv.js.zip -u$1:$2 https://api.bintray.com/content/daemoohn/libvorbis-ogv.js/libvorbis-ogv.js/$version/libvorbis-ogv.js.zip?publish=1
