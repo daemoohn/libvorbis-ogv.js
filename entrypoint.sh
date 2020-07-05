b511c039bc2ab666394ca9cbe21c34b3fae479fb
Bogdan Ungureanu <49443238+daemoohn@users.noreply.github.com>
2020-07-06 01:57:16 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,6 +15,45 @@ if [[ $bintray_response != *"Version '$version' was not found"* ]]; then
   exit 1
 fi
 
+## getting ogg source code
+git clone https://gitlab.xiph.org/xiph/ogg.git
+
+## configureOgg.sh
+
+cd ogg
+if [ ! -f configure ]; then
+  # generate configuration script
+  sed -i.bak 's/$srcdir\/configure/#/' autogen.sh
+  ./autogen.sh
+fi
+cd ..
+
+## compileOggJs.sh
+dir=`pwd`
+
+# set up the build directory
+mkdir -p build
+cd build
+
+mkdir -p js
+cd js
+
+mkdir -p root
+mkdir -p libogg
+cd libogg
+
+# finally, run configuration script
+emconfigure ../../../ogg/configure \
+    --prefix="$dir/build/js/root" \
+    --disable-shared \
+|| exit 1
+
+# compile libogg
+emmake make -j4 || exit 1
+emmake make install || exit 1
+
+cd $dir
+
 ## configureVorbis.sh
 cd vorbis
 if [ ! -f configure ]; then
